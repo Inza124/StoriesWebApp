8c63bc1c53caf7d2f6201678585c82d2b9b75c61
Inza124 <piotrstachaczynski.prv@gmail.com>
2017-10-07 18:15:40 +0200

    Backend Structure #1
    create method, dbo, show

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,14 +1,25 @@
 class StoriesController < ApplicationController
 
   def index
+      @stories = Story.all
+  end
 
+  def show
+      @story = Story.find(params[:id])
   end
 
   def new
 
   end
 
-  def Create
-
+  def create
+      @story = Story.new(story_params)
+      @story.save
+      redirect_to @story
   end
+
+  private def story_params
+    params.require(:story).permit(:title, :auth, :body)
+  end
+
 end
