413be3385a853549a58ba7d0a5878dc18a371fec
Inza124 <piotrstachaczynski.prv@gmail.com>
2017-10-11 00:07:22 +0200

    CRUD users #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'stories#index'
 
 get 'about' => 'pages#about'
+match '/users/:id',     to: 'users#show',       via: 'get'
+
+resources :users, :only =>[:show]
 resources :stories
+
 end
