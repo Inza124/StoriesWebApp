5058c7b068f4057fc3389cb9e5660254b12e703e
Inza124 <piotrstachaczynski.prv@gmail.com>
2017-10-10 13:35:28 +0200

    FrontEnd edt

--- db/migrate/20171009153710_devise_create_users.rb
+++ db/migrate/20171009153710_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :name, null: false, default: ""
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -20,7 +20,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       t.string   :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
+      # t.string   :confirmation_tokenaas
       # t.datetime :confirmed_at
       # t.datetime :confirmation_sent_at
       # t.string   :unconfirmed_email # Only if using reconfirmable
@@ -36,7 +36,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
+    add_index :users, :name,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
 end
