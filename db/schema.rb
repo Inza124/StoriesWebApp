f0141967fa96ab71c112b4af304238daec1e6ed8
Inza124 <Inza124@users.noreply.github.com>
2018-09-17 23:20:08 +0200

    deploy heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171015121620) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.integer "story_id"
